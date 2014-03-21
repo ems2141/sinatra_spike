c54184dbaef4ef5373d3b142e2e6f5eb7ce41858
Rachel Logie <rachel.logie1@gmail.com>
2014-03-21 10:31:13 -0600

    updated website to include filter
    We are now able to filter items by the whole item name or characters contained within the name of the item.
    
    Signed-off-by: Ellie Schneiders <ems2141@gmail.com>

--- app.rb
+++ app.rb
@@ -1,11 +1,25 @@
 require 'sinatra'
 require './lib/item'
+require './lib/item_container'
 
 get '/' do
   erb :index
 end
 
 get '/items' do
+  filter = params[:filter]
+  array_of_items = ItemContainer.new.menu
+
+  @displayed_items = []
+  array_of_items.each do |item|
+    if item.name.downcase.include? filter.to_s
+      @displayed_items << item
+    end
+  end
+
+  if @displayed_items.empty?
+    @displayed_items = array_of_items
+  end
   erb :items
 end
 
---
+++ lib/item_container.rb
@@ -0,0 +1,9 @@
+class ItemContainer
+  def menu
+    list_of_items=
+      [Item.new(1, "Spinach"),
+       Item.new(2, "Chicken"),
+       Item.new(3, "Asparagus")]
+    list_of_items
+  end
+end
